6c5ccc1d07a1d72b91f89445e76d8288f5add58c
Felip Ladron <fladron@gmail.com>
2014-11-23 18:59:26 +0100

    New routing
    Created Homepage, Donors page and Results page

--- src/controllers.php
+++ src/controllers.php
@@ -8,12 +8,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
-})
-->bind('homepage')
-;
-
+// errors
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
         return;
@@ -28,4 +23,67 @@ $app->error(function (\Exception $e, $code) use ($app) {
     );
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
+
+// homepage
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html', array());
+})->bind('homepage');
+
+// donors page
+$app->get('/donors', function () use ($app) {
+    $sql = "SELECT paymentID, amount/100 AS money, firstname, lastname FROM blog_fullstripe_payments";
+    $single_donors = $app['db']->fetchAssoc($sql);
+
+    $sql = "SELECT * FROM blog_fullstripe_subscribers";
+    $subscription_donors = $app['db']->fetchAssoc($sql);
+
+    $vars = array(
+      'single_donors' => $single_donors,
+      'subscription_donors' => $subscription_donors,
+    );
+    return $app['twig']->render('donors.html', $vars);
+});
+
+// results page
+$app->get('/results', function () use ($app) {
+    $sql = "SELECT SUM(amount)/100 AS money FROM blog_fullstripe_payments";
+    $payments_money = $app['db']->fetchAssoc($sql);
+
+    $sql = "SELECT planID AS plan, 
+                   COUNT(*) AS number,
+                   COUNT(*) * (
+                     CASE planID 
+                       WHEN 'monthly5' THEN 5 
+                       WHEN 'monthly10' THEN 10 
+                       WHEN 'monthly20' THEN 20 
+                       WHEN 'monthly30' THEN 30 
+                       WHEN 'monthly50' THEN 50 
+                     END
+                   ) AS money
+            FROM blog_fullstripe_subscribers 
+            WHERE planID <> '' GROUP BY planID";
+    $subscriptions = $app['db']->fetchAll($sql);
+
+    $sql = "SELECT COUNT(*) AS number FROM blog_fullstripe_subscribers WHERE planID <> ''";
+    $subscriptions_number = $app['db']->fetchAssoc($sql);
+
+    $sql = "SELECT SUM(
+              CASE planID 
+                WHEN 'monthly5' THEN 5 
+                WHEN 'monthly10' THEN 10 
+                WHEN 'monthly20' THEN 20 
+                WHEN 'monthly30' THEN 30 
+                WHEN 'monthly50' THEN 50 
+              END
+            ) AS money FROM blog_fullstripe_subscribers";
+    $subscriptions_money = $app['db']->fetchAssoc($sql);
+
+    $vars = array(
+      'payments_money' => intval($payments_money['money']),
+      'subscriptions' => $subscriptions,
+      'subscriptions_number' => $subscriptions_number['number'],
+      'subscriptions_money' => $subscriptions_money['money'],
+    );
+    return $app['twig']->render('results.html', $vars);
 });

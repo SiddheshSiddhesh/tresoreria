4fe88fe05c3487862989a9bf0a1315535d5ec0ec
Felip Ladron <fladron@gmail.com>
2014-11-23 22:57:20 +0100

    Major changes for all routes

--- src/controllers.php
+++ src/controllers.php
@@ -32,22 +32,64 @@ $app->get('/', function () use ($app) {
 
 // donors page
 $app->get('/donors', function () use ($app) {
-    $sql = "SELECT paymentID, amount/100 AS money, firstname, lastname FROM blog_fullstripe_payments";
-    $single_donors = $app['db']->fetchAssoc($sql);
-
-    $sql = "SELECT * FROM blog_fullstripe_subscribers";
-    $subscription_donors = $app['db']->fetchAssoc($sql);
+    $sql = "SELECT paymentID, FLOOR(amount/100) AS money, firstname, lastname, created FROM blog_fullstripe_payments WHERE eventID = ? ORDER BY created DESC";
+    $single_donors = $app['db']->fetchAll($sql, array('BANK ACCOUNT PAYMENT'));
 
     $vars = array(
+      'detail_uri' => '/donors/single/',
       'single_donors' => $single_donors,
+    );
+    return $app['twig']->render('donors_single.html', $vars);
+});
+$app->get('/donors/subscribers', function () use ($app) {
+    $sql = "SELECT subscriberID, planID, firstname, lastname, created FROM blog_fullstripe_subscribers WHERE stripeCustomerID = ? ORDER BY created DESC";
+    $subscription_donors = $app['db']->fetchAll($sql, array('BANK ACCOUNT PAYMENT'));
+
+    $vars = array(
+      'detail_uri' => '/donors/subscriber/',
       'subscription_donors' => $subscription_donors,
     );
-    return $app['twig']->render('donors.html', $vars);
+    return $app['twig']->render('donors_subscription.html', $vars);
+});
+
+// donor detail
+$app->get('/donors/{type}/{id}', function ($type, $id) use ($app) {
+    if ($type == 'single'){
+      $sql = "SELECT FLOOR(amount/100) AS money, firstname, lastname, email, telephone, documentType, documentID, birthDate, addressCountry, addressLine1, addressCity, addressState, addressZip, created, bankCCC, bankIBAN, bankBIC 
+              FROM blog_fullstripe_payments
+              WHERE paymentID = ?";
+    }else if ($type == 'subscriber'){
+      $sql = "SELECT planID, firstname, lastname, email, telephone, documentType, documentID, birthDate, addressCountry, addressLine1, addressCity, addressState, addressZip, created, bankCCC, bankIBAN, bankBIC 
+              FROM blog_fullstripe_subscribers
+              WHERE subscriberID = ?";
+    }
+    $detail = $app['db']->fetchAssoc($sql, array((int) $id));
+
+    // decrypt encrypted data
+    $rsa = new Crypt_RSA();
+    $private_key = file_get_contents(PRIVATE_KEY_PATH);
+    $rsa->loadKey($private_key);
+    foreach ($detail as $key => $value) {
+      switch ($key){
+        case 'bankCCC':
+        case 'bankIBAN':
+        case 'bankBIC':
+          if ($value !== 'BANK FIELD NOT FILLED'){
+            $detail[$key] = $rsa->decrypt($value);
+          }
+          break;
+      }
+    }
+
+    $vars = array(
+      'detail' => $detail,
+    );
+    return $app['twig']->render('donors_detail.html', $vars);
 });
 
 // results page
 $app->get('/results', function () use ($app) {
-    $sql = "SELECT SUM(amount)/100 AS money FROM blog_fullstripe_payments";
+    $sql = "SELECT FLOOR(SUM(amount)/100) AS money FROM blog_fullstripe_payments";
     $payments_money = $app['db']->fetchAssoc($sql);
 
     $sql = "SELECT planID AS plan, 
@@ -80,7 +122,7 @@ $app->get('/results', function () use ($app) {
     $subscriptions_money = $app['db']->fetchAssoc($sql);
 
     $vars = array(
-      'payments_money' => intval($payments_money['money']),
+      'payments_money' => $payments_money['money'],
       'subscriptions' => $subscriptions,
       'subscriptions_number' => $subscriptions_number['number'],
       'subscriptions_money' => $subscriptions_money['money'],
